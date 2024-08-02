26ff0160a5f2a8a8fafa7eb864ba747e417e21cf
Kristina <cauliflowerpineapple@gmail.com>
2024-08-02 17:20:51 -0400

    responsiveness improvements

--- qweb/src/Contact.js
+++ qweb/src/Contact.js
@@ -29,39 +29,29 @@ function Contact(){
     }
 
 
+    const sendEmail = (e) => {
+        e.preventDefault();
 
-    // function getInput() {
-    //     console.log("Name: ", name);
-    //     console.log("Email: ", email);
-    //     console.log("Message: ", msg);
-
-    // }
-
-    // const sendEmail = (e) => {
-    //     e.preventDefault();
-
-    //     emailjs
-    //         .sendForm()
-
-    // };
-    // const form = useRef();
-
-    // const sendEmail = (e) => {
-    //     e.preventDefault();
-
-    //     emailjs
-    //     .sendForm("service_iu0bbyc", "template_u8nr4hx", form.current, {
-    //       publicKey: 'wq7X-oGFhTMDET33f',
-    //     })
-    //     .then(
-    //       () => {
-    //         console.log('SUCCESS!');
-    //       },
-    //       (error) => {
-    //         console.log('FAILED...', error.text);
-    //       },
-    //     );
-    // };
+        emailjs.send(
+            "service_iu0bbyc", 
+            "template_u8nr4hx",
+            {
+                to_name: "Queen's University Art Club",
+                user_name: name,
+                user_email: email,
+                message: msg,
+            },
+            "wq7X-oGFhTMDET33f"
+        )
+        .then((response) => {
+            console.log('SUCCESS!', response.status, response.text);
+            alert("Email sent successfully!");
+          }, (error) => {
+            console.log('FAILED...', error.text);
+            alert("Failed to send email.");
+          },
+        );
+    };
     
 
 
@@ -71,116 +61,114 @@ function Contact(){
             <h1>Contact.</h1>
             
             <div className='row' id='row'>
-                <form>
-                    <div className='inputFields' id='purpleTextbox'>
-                        {/* Name textfield */}
-                        <TextField 
-                            id="outlined-basic"
-                            label="Name"
-                            variant="outlined"
-                            value={name}
-                            onChange={handleName}
-                            sx = {{
-                                "& .MuiOutlinedInput-root" : {
-                                    background: "#EDECF0",
-                                    color: "#1a1a1a",
-                                    fontFamily: "Helvetica",
-                                    width: "350px",
-                                    "& .MuiOutlinedInput-notchedOutline" : {
-                                        borderColor: "#A496BC",
-                                        borderWidth: "2px",
-                                    },
+                <div className='inputFields' id='purpleTextbox'>
+                    {/* Name textfield */}
+                    <TextField 
+                        id="outlined-basic"
+                        label="Name"
+                        variant="outlined"
+                        value={name}
+                        onChange={handleName}
+                        sx = {{
+                            "& .MuiOutlinedInput-root" : {
+                                background: "#EDECF0",
+                                color: "#1a1a1a",
+                                fontFamily: "Helvetica",
+                                width: "350px",
+                                "& .MuiOutlinedInput-notchedOutline" : {
+                                    borderColor: "#A496BC",
+                                    borderWidth: "2px",
                                 },
-                                "& .MuiInputLabel-outlined" : {
-                                    color: "#766B94",
-                                    fontWeight: "bold",
+                            },
+                            "& .MuiInputLabel-outlined" : {
+                                color: "#766B94",
+                                fontWeight: "bold",
+                            },
+                            "& .Mui-focused" : {
+                                "& .MuiOutlinedInput-notchedOutline" : {
+                                    borderColor: "#766B94",
+                                    borderWidth: "2px",
                                 },
-                                "& .Mui-focused" : {
-                                    "& .MuiOutlinedInput-notchedOutline" : {
-                                        borderColor: "#766B94",
-                                        borderWidth: "2px",
-                                    },
+                            },
+                            
+                        }}
+                    />
+                    {/* email textfield */}
+                    <TextField 
+                        id="outlined-basic"
+                        label="E-mail"
+                        variant="outlined"
+                        value={email}
+                        onChange={handleEmail}
+                        sx = {{
+                            "& .MuiOutlinedInput-root" : {
+                                background: "#EDECF0",
+                                color: "#1a1a1a",
+                                fontFamily: "Helvetica",
+                                width: "350px",
+                                "& .MuiOutlinedInput-notchedOutline" : {
+                                    borderColor: "#A496BC",
+                                    borderWidth: "2px",
                                 },
+                            },
+                            "& .MuiInputLabel-outlined" : {
+                                color: "#766B94",
+                                fontWeight: "bold",
+                            },
+                            "& .Mui-focused" : {
+                                "& .MuiOutlinedInput-notchedOutline" : {
+                                    borderColor: "#766B94",
+                                    borderWidth: "2px",
+                                },
+                            },
+                            
+                        }}
+                    />
+                    {/* message textbox */}
+                    <TextField 
+                        id="outlined-basic"
+                        label="Message"
+                        variant="outlined"
+                        multiline
+                        rows = {5}
+                        value={msg}
+                        onChange={handleMsg}
+                        sx = {{
+                            "& .MuiInputBase-root" : {
+                                alignItems: "flex-start",
+                            },
+                            "& .MuiInputBase-input" : {
+                                textAlign: "left",
+                                wordBreak: "break-word",
+                            },
+                            "& .MuiOutlinedInput-root" : {
+                                background: "#EDECF0",
+                                color: "#1a1a1a",
+                                fontFamily: "Helvetica",
+                                width: "350px",
+                                height: "200px",
                                 
-                            }}
-                        />
-                        {/* email textfield */}
-                        <TextField 
-                            id="outlined-basic"
-                            label="E-mail"
-                            variant="outlined"
-                            value={email}
-                            onChange={handleEmail}
-                            sx = {{
-                                "& .MuiOutlinedInput-root" : {
-                                    background: "#EDECF0",
-                                    color: "#1a1a1a",
-                                    fontFamily: "Helvetica",
-                                    width: "350px",
-                                    "& .MuiOutlinedInput-notchedOutline" : {
-                                        borderColor: "#A496BC",
-                                        borderWidth: "2px",
-                                    },
+                                "& .MuiOutlinedInput-notchedOutline" : {
+                                    borderColor: "#A496BC",
+                                    borderWidth: "2px",
                                 },
-                                "& .MuiInputLabel-outlined" : {
-                                    color: "#766B94",
-                                    fontWeight: "bold",
+                            },
+                            "& .MuiInputLabel-outlined" : {
+                                color: "#766B94",
+                                fontWeight: "bold",
+                            },
+                            "& .Mui-focused" : {
+                                "& .MuiOutlinedInput-notchedOutline" : {
+                                    borderColor: "#766B94",
+                                    borderWidth: "2px",
                                 },
-                                "& .Mui-focused" : {
-                                    "& .MuiOutlinedInput-notchedOutline" : {
-                                        borderColor: "#766B94",
-                                        borderWidth: "2px",
-                                    },
-                                },
-                                
-                            }}
-                        />
-                        {/* message textbox */}
-                        <TextField 
-                            id="outlined-basic"
-                            label="Message"
-                            variant="outlined"
-                            multiline
-                            rows = {5}
-                            fullWidth
-                            value={msg}
-                            onChange={handleMsg}
-                            sx = {{
-                                "& .MuiInputBase-root" : {
-                                    alignItems: "flex-start",
-                                },
-                                "& .MuiInputBase-input" : {
-                                    textAlign: "left",
-                                    wordBreak: "break-word",
-                                },
-                                "& .MuiOutlinedInput-root" : {
-                                    background: "#EDECF0",
-                                    color: "#1a1a1a",
-                                    fontFamily: "Helvetica",
-                                    width: "350px",
-                                    height: "200px",
-                                    
-                                    "& .MuiOutlinedInput-notchedOutline" : {
-                                        borderColor: "#A496BC",
-                                        borderWidth: "2px",
-                                    },
-                                },
-                                "& .MuiInputLabel-outlined" : {
-                                    color: "#766B94",
-                                    fontWeight: "bold",
-                                },
-                                "& .Mui-focused" : {
-                                    "& .MuiOutlinedInput-notchedOutline" : {
-                                        borderColor: "#766B94",
-                                        borderWidth: "2px",
-                                    },
-                                },
-                                
-                            }}
-                        />
-                        <div className='button-div'><button class="submit-btn">Submit</button></div>
-                    </div>
-                </form>
+                            },
+                            
+                        }}
+                    />
+                    <div className='button-div'><button class="submit-btn" onClick={sendEmail}>Submit</button></div>
+                </div>
+            
                 <div className="purpleTextbox" id="purpleTextbox">
                     <h2>Stay up to date with us.</h2>
                     <div className='logo_link'>
@@ -195,10 +183,7 @@ function Contact(){
                         <img src="https://cdn1.iconfinder.com/data/icons/ui-essential-17/32/UI_Essential_Outline_1_essential-app-ui-picture-photo-512.png" alt="Photo icon" className="contact_img"></img>
                         <p>See our past showcases.</p>
                     </div>
-                    
-
                 </div>
-
             </div>
             <footer>
                 <Footer />
